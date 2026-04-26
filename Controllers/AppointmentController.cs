720e63537abb74d268bbd160b80c0c6baa5738f0
Sebastian Kurek <sebastiankurekmdp@gmail.com>
2026-04-26 16:46:41 +0200

    Implement GET appointments list

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -1,6 +1,56 @@
-﻿namespace apbd_s34366_cw7.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
-public class AppointmentController
+namespace apbd_s34366_cw7.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentController : ControllerBase
 {
-    //TODO: implement controller for appointments
+    private readonly IConfiguration _configuration;
+    
+    public AppointmentController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAppointments()
+    {
+        await using var conn = new SqlConnection(GetConnectionString());
+    
+        var sql = "SELECT a.IdAppointment, a.AppointmentDate, a.Status, " +
+                  "p.FirstName + ' ' + p.LastName AS PatientName, " +
+                  "d.FirstName + ' ' + d.LastName AS DoctorName " +
+                  "FROM Appointments a " +
+                  "JOIN Patients p ON a.IdPatient = p.IdPatient " +
+                  "JOIN Doctors d ON a.IdDoctor = d.IdDoctor";
+
+        var cmd = new SqlCommand(sql, conn);
+    
+        var list = new List<object>();
+    
+        await conn.OpenAsync();
+    
+        await using var r = await cmd.ExecuteReaderAsync();
+    
+        while (await r.ReadAsync())
+        {
+            list.Add(new { 
+                IdAppointment = r["IdAppointment"], 
+                Date = r["AppointmentDate"], 
+                Status = r["Status"],
+                Patient = r["PatientName"],
+                Doctor = r["DoctorName"]
+            });
+        }
+
+        return Ok(list);
+    }
+    
+    private string GetConnectionString()
+    {
+        return _configuration.GetConnectionString("DefaultConnection") 
+               ?? "Server=localhost,1433;Database=ClinicAdoNet;User Id=sa;Password=YourStrong@Passw0rd;TrustServerCertificate=True";
+    }
 }
