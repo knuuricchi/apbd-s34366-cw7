281c2b266f4df149b4effba221ef80b19fd179bb
Sebastian Kurek <sebastiankurekmdp@gmail.com>
2026-04-26 16:12:03 +0200

    Create required DTOs

---
+++ DTOs/AppointmentDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace apbd_s34366_cw7.DTOs;
+
+public class AppointmentDetailsDto
+{
+    public int IdAppointment { get; set; }
+    public DateTime AppointmentDate { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public string Reason { get; set; } = string.Empty;
+    public string PatientFullName { get; set; } = string.Empty;
+    public string PatientEmail { get; set; } = string.Empty;
+    public string DoctorFullName { get; set; } = string.Empty;
+    public string LicenseNumber { get; set; } = string.Empty;
+    public string? InternalNotes { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
---
+++ DTOs/ErrorResponseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace apbd_s34366_cw7.DTOs;
+
+public class ErrorResponseDto
+{
+    public string Message { get; set; } = string.Empty;
+}
