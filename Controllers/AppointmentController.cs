b4db276f4848f85aa7e30873a7b9a61adb0a7dca
Sebastian Kurek <sebastiankurekmdp@gmail.com>
2026-04-26 16:17:18 +0200

    Add project structure and inital classes

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,6 @@
+﻿namespace apbd_s34366_cw7.Controllers;
+
+public class AppointmentController
+{
+    //TODO: implement controller for appointments
+}
---
+++ Services/AppointmentService.cs
@@ -0,0 +1,6 @@
+﻿namespace apbd_s34366_cw7.Services;
+
+public class AppointmentService
+{
+    //todo: implement service for appointments
+}
